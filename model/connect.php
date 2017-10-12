1e79d204d75ef9e6d4cb61f4177cbb1f01c60047
huong huong <huongnguyen08.cv@gmail.com>
2017-10-12 19:13:05 +0700

    buoi1

---
+++ model/connect.php
@@ -0,0 +1,56 @@
+<?php
+
+class Connect{
+
+	public $_mysqli;
+	public $_sql;
+
+	public function __construct(){
+
+		$this->_mysqli = new mysqli("localhost",'root','','php1307');
+
+		if($this->_mysqli->connect_errno){
+			echo "Lỗi kết nối: ".$this->_mysqli->connect_error;
+			die;
+		}
+	}
+
+	public function setQuery($sql){
+		$this->_sql = $sql;
+	}
+
+	public function execute(){
+		$result = $this->_mysqli->query($this->_sql);
+		return $result;
+	}
+
+	public function loadAllRows(){
+		$execute = $this->execute();
+		$dataList = [];
+		if($execute){
+			while($row = $execute->fetch_object()){
+				array_push($dataList, $row);
+			}
+		}
+		return $dataList;
+	}
+
+	public function loadRow(){
+		$execute = $this->execute();
+		$row = false;
+		if($execute){
+			$row = $execute->fetch_object();
+		}
+		return $row;
+	}
+
+	public function getLastId(){
+		return $this->_mysqli->insert_id;
+	}
+
+	public function disconnect(){
+		$this->_mysqli = NULL;
+	}
+}
+
+?>
